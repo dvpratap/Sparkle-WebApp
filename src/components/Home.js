9197b38f7e51b5ccc139d417827958ad640d0b5b
drajawat <dv269119@dal.ca>
2023-02-08 23:44:48 -0400

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import './Parent.css'
 const Home = () => {
   return (
     <div>
-      <h2 className='div-style'> Sparkle!... Premium Jewelry </h2>
-      <h2 className='div-style'> Click on Billing Navbar </h2>
+      <h2 className='div-home'> Sparkle!... Premium Jewelry </h2>
+      <h2 className='div-home'> Click on Billing Navbar </h2>
     </div>
   );
 };
